75012a4d5d7bab073621dd9df54e03dcd67937ff
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-06-09 14:38:39 +0500

    Comments Updated

--- 32-Least_Common_Multiple.py
+++ 32-Least_Common_Multiple.py
@@ -1,4 +1,4 @@
-# Ai Assignment 32 - Compute The Least Common Multiple of Two Positive Integers
+# Ai Assignment 32 - Compute the Least Common Multiple of Two Positive Integers
 n1 = int(input("Input Integer # 1 : "))
 n2 = int(input("Input Integer # 2 : "))
 x = n1
