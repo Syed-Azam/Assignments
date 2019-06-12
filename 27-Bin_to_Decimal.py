eeaf49fc807c450c43a502a5276d59377f3f10ba
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-06-12 12:43:32 +0500

    Update Logic

--- 27-Bin_to_Decimal.py
+++ 27-Bin_to_Decimal.py
@@ -4,4 +4,8 @@ for c in b: # Check if user input is really a binary number
     if not (c == "0" or c == "1"):
         print("That's not a Binary Number!")
         quit()
-print("Decimal is :", int(b, 2))
+l = len(b)
+d = 0
+for c in range(l):
+    d = d + int(b[c]) * (2**(l-c-1))
+print("Decimal Equavalent is : ", d)
