e1c750ae71659e784e697f9d9efca44f19085e30
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-07-20 12:16:06 +0500

    X+Y Formula

--- 14-X_plus_Y_Formula.py
+++ 14-X_plus_Y_Formula.py
@@ -1,4 +1,6 @@
 # Ai Assignment 14 - (x + y) * (x + y)
 x = int(input("Enter Value of X : "))
 y = int(input("Enter Value of Y : "))
-print((x + y) * (x - y))
+print((x + y) * (x + y))
+print( (x**2) + ( 2 * x * y ) + (y**2) ) # using formula
+input()
